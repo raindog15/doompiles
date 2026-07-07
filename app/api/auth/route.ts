0165a7212c25435a94d59f7ef944aa4835a7a370
Nicole Mansfield <mansfield.nicole@gmail.com>
2026-07-06 23:30:34 -0700

    fix build error

--- app/api/auth/route.ts
+++ app/api/auth/route.ts
@@ -1,3 +1,20 @@
 import { auth } from '@/lib/auth/server';
+import { NextRequest } from 'next/server';
 
-export const { GET, POST } = auth.handler();
+const handler = auth.handler();
+
+type Params = { path: string[] };
+
+export async function GET(
+  request: NextRequest,
+  context: { params: Promise<Params> }
+) {
+  return handler.GET(request, context);
+}
+
+export async function POST(
+  request: NextRequest,
+  context: { params: Promise<Params> }
+) {
+  return handler.POST(request, context);
+}
