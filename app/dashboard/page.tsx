2ae02561c87856ebdfc5ad1ab0a1fa3fae733aa3
Nicole Mansfield <mansfield.nicole@gmail.com>
2026-07-20 15:25:28 -0700

    cleaned up dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,24 +1,20 @@
 import { auth } from '@/lib/auth/server';
 import { redirect } from 'next/navigation';
 import DashboardClient from './DashboardClient';
-import { cookies } from 'next/headers';
+import { cookies } from 'next/headers'; // these are load bearing 
 
 export const dynamic = 'force-dynamic';
 
 export default async function Dashboard() {
-  const cookieStore = await cookies();
-  const allCookies = cookieStore.getAll();
-  console.log('cookies on server:', JSON.stringify(allCookies.map(c => c.name)));
+
+  await cookies();  // these are load bearing. getSession will not work without them.
 
   const { data: session } = await auth.getSession();
 
-console.log('session:', JSON.stringify(session));
-
-  
   if (!session?.user) {
-    return <h1>Not logged in</h1>;
-  } else {
-    return <DashboardClient user={session.user} />;
+    redirect('/');
   }
-  
+
+    return <DashboardClient user={session.user} />;
+
 }
