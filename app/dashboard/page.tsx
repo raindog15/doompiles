e230b7b3f86429c374e618c57d75471dc519ac05
Nicole Mansfield <mansfield.nicole@gmail.com>
2026-07-20 23:22:49 -0700

    put some more things back

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,7 +2,7 @@ import { cookies } from 'next/headers'; // these are load bearing
 import { auth } from '@/lib/auth/server';
 import { redirect } from 'next/navigation';
 import DashboardClient from './DashboardClient';
-import { getUser } from '@/lib/db/users';
+//import { getUser } from '@/lib/db/users';
 
 export const dynamic = 'force-dynamic';
 
@@ -19,16 +19,16 @@ export default async function Dashboard() {
     redirect('/');
   }
 
-  console.log('has db url:', !!process.env.DATABASE_URL);
+  //console.log('has db url:', !!process.env.DATABASE_URL);
 
   console.log('pre getUser call');
-  const user = await getUser(session.user.id);
+//  const user = await getUser(session.user.id);
   console.log('post getUser call');
 
     return (
       <DashboardClient
         user={session.user}
-        needsHouseHold={!user?.household_id} 
+        needsHouseHold={true}
         />
     );
 }
--- proxy.ts
+++ proxy.ts
@@ -1,10 +1,8 @@
 import { auth } from '@/lib/auth/server';
-import { NextRequest } from 'next/server';
 
-export default async function middleware(request: NextRequest) {
-  console.log('middleware hit:', request.nextUrl.pathname);
-  return auth.middleware({ loginUrl: '/' })(request);
-}
+export default auth.middleware({
+  loginUrl: '/',
+});
 
 export const config = {
   matcher: [
