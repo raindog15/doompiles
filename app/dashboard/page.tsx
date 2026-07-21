3fcad3bdf6c7d18ef031b5969abd27fe3c1512c0
raindog15 <37352904+raindog15@users.noreply.github.com>
2026-07-20 23:35:02 -0700

    put dashboard back to working state

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,34 +1,24 @@
-import { cookies } from 'next/headers'; // these are load bearing 
 import { auth } from '@/lib/auth/server';
 import { redirect } from 'next/navigation';
 import DashboardClient from './DashboardClient';
-//import { getUser } from '@/lib/db/users';
+import { cookies } from 'next/headers';
 
 export const dynamic = 'force-dynamic';
 
 export default async function Dashboard() {
-
-  const cookieStore = await cookies(); // load bearing cookies!
+  const cookieStore = await cookies();
   const allCookies = cookieStore.getAll();
   console.log('cookies on server:', JSON.stringify(allCookies.map(c => c.name)));
 
   const { data: session } = await auth.getSession();
-  console.log('post session check, session:', JSON.stringify(session));
 
+console.log('session:', JSON.stringify(session));
+
+  
   if (!session?.user) {
-    redirect('/');
+    return <h1>Not logged in</h1>;
+  } else {
+    return <DashboardClient user={session.user} />;
   }
-
-  //console.log('has db url:', !!process.env.DATABASE_URL);
-
-  console.log('pre getUser call');
-//  const user = await getUser(session.user.id);
-  console.log('post getUser call');
-
-    return (
-      <DashboardClient
-        user={session.user}
-        needsHouseHold={true}
-        />
-    );
+  
 }
