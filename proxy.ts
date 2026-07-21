07a97102ed471b3214acf7d5d90b5876dd8a8772
Nicole Mansfield <mansfield.nicole@gmail.com>
2026-07-20 23:07:22 -0700

    add middleware logging

--- proxy.ts
+++ proxy.ts
@@ -1,8 +1,10 @@
 import { auth } from '@/lib/auth/server';
+import { NextRequest } from 'next/server';
 
-export default auth.middleware({
-  loginUrl: '/',
-});
+export default async function middleware(request: NextRequest) {
+  console.log('middleware hit:', request.nextUrl.pathname);
+  return auth.middleware({ loginUrl: '/' })(request);
+}
 
 export const config = {
   matcher: [
