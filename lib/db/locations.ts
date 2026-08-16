4ca74dd99de4dc01338b4e0705e264eda4358ba5
raindog15 <37352904+raindog15@users.noreply.github.com>
2026-08-15 22:55:11 -0700

    Add getLocation function to locations.ts

--- lib/db/locations.ts
+++ lib/db/locations.ts
@@ -19,16 +19,29 @@ export async function getLocationsByHousehold(householdId: number) {
   return rows;
 }
 
+export async function getLocationId(name: string, householdId: number){
+  const rows = await sql`
+    select
+      location_id from locations
+    where household_id = ${household_id}
+      and name = ${name}
+    and deleted_at is null
+    limit 1
+    `;
+  return rows;
+}
+
 export async function createLocation(
     name: string, 
     category: string, 
     floor: number, 
-    parent_location: number) {
+    parent_location: number,
+    household_id: number) {
   const rows = await sql`
     insert into locations
-      (name, category, floor, parent_location)
+      (name, category, floor, parent_location, household_id)
     values
-      (${name}, ${category}, ${floor}, ${parent_location})
+      (${name}, ${category}, ${floor}, ${parent_location}, ${household_id})
       returning location_id, name, category, floor, parent_location
       `;
   return rows
